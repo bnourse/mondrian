a27b3379708afbf869069a0da3ff435fa0ddc660
Ben Nourse <bnourse@gmail.com>
2017-02-13 20:48:46 -0600

    wtf

--- controller.rb
+++ controller.rb
@@ -7,16 +7,16 @@ set :server, 'thin'
 
 ms = MondrianServer.new
 
-# options("/save") do
-#   response.headers["Access-Control-Allow-Origin"] = "*"
-#   response.headers["Access-Control-Allow-Methods"] = "POST"
-# end
+options("/save") do
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "POST"
+end
 
-# post("/save") do 
-# 	request.body.rewind
-# 	@json = JSON.parse(request.body.read)
-# 	ms.save(@json)
-# end
+post("/save") do 
+	request.body.rewind
+	@json = JSON.parse(request.body.read)
+	ms.save(@json)
+end
 
 options '/load' do
   response.headers["Access-Control-Allow-Origin"] = "*"
